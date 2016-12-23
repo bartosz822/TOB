29fba91746010dc91df43d9e3a52d9736df4ee91
Bartosz Radzyński <radzynskib@gmail.com>
2016-12-23 01:19:51 +0100

    init

---
+++ app/Main.hs
@@ -0,0 +1,106 @@
+module Main where
+
+import Lib
+
+--Just for testing
+import Data.List
+import Network
+import System.IO
+import System.Exit
+import Control.Arrow
+import Control.Monad.Reader
+import Control.Exception
+import Text.Printf
+import System.Time
+
+server = "irc.freenode.org"
+port   = 6667
+chan   = "#BRbotTesting"
+nick   = "BRbot"
+
+-- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
+type Net = ReaderT Bot IO
+data Bot = Bot { socket :: Handle, starttime :: ClockTime }
+
+-- Set up actions to run on start and end, and run the main loop
+main :: IO ()
+main = bracket connect disconnect loop
+  where
+    disconnect = hClose . socket
+    loop st    = runReaderT run st
+
+-- Connect to the server and return the initial bot state
+connect :: IO Bot
+connect = notify $ do
+    t <- getClockTime
+    h <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering h NoBuffering
+    return (Bot h t)
+  where
+    notify a = bracket_
+        (printf "Connecting to %s ... " server >> hFlush stdout)
+        (putStrLn "done.")
+        a
+
+-- We're in the Net monad now, so we've connected successfully
+-- Join a channel, and start processing commands
+run :: Net ()
+run = do
+    write "NICK" nick
+    write "USER" (nick++" 0 * :tutorial bot")
+    write "JOIN" chan
+    asks socket >>= listen
+
+-- Process each line from the server
+listen :: Handle -> Net ()
+listen h = forever $ do
+    s <- init `fmap` io (hGetLine h)
+    io (putStrLn s)
+    if ping s then pong s else eval (clean s)
+  where
+    forever a = a >> forever a
+    clean     = drop 1 . dropWhile (/= ':') . drop 1
+    ping x    = "PING :" `isPrefixOf` x
+    pong x    = write "PONG" (':' : drop 6 x)
+
+-- Dispatch a command
+eval :: String -> Net ()
+eval     "!quit"               = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval x | "!id " `isPrefixOf` x = privmsg (drop 4 x)
+eval "!uptime" = uptime >>= privmsg
+eval     _                     = return () -- ignore everything else
+
+uptime :: Net String
+uptime = do
+    now  <- io getClockTime
+    zero <- asks starttime
+    return . pretty $ diffClockTimes now zero
+
+-- Send a privmsg to the current chan + server
+privmsg :: String -> Net ()
+privmsg s = write "PRIVMSG" (chan ++ " :" ++ s)
+
+--
+-- Pretty print the date in '1d 9h 9m 17s' format
+--
+pretty :: TimeDiff -> String
+pretty td =
+  unwords $ map (uncurry (++) . first show) $
+  if null diffs then [(0,"s")] else diffs
+  where merge (tot,acc) (sec,typ) = let (sec',tot') = divMod tot sec
+                                    in (tot',(sec',typ):acc)
+        metrics = [(86400,"d"),(3600,"h"),(60,"m"),(1,"s")]
+        diffs = filter ((/= 0) . fst) $ reverse $ snd $
+                foldl' merge (tdSec td,[]) metrics
+
+
+-- Send a message out to the server we're currently connected to
+write :: String -> String -> Net ()
+write s t = do
+    h <- asks socket
+    io $ hPrintf h "%s %s\r\n" s t
+    io $ printf    "> %s %s\n" s t
+
+-- Convenience.
+io :: IO a -> Net a
+io = liftIO
